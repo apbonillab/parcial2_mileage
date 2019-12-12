6c6e080ac52a40addc81e3a2933bc17b8df19fcf
Adriana Bonilla <adrianabonilla@MacBook-Pro-de-Adriana.local>
2019-12-11 19:31:36 -0500

    pruebas

--- app/controllers/worker.ctrl.js
+++ app/controllers/worker.ctrl.js
@@ -5,10 +5,10 @@ var randomService = require('../services/random.srv.js');
 var http = require('http');
 
   
-router.post('/test', (req,res) => {
-
+router.post('/calabash', (req,res) => {
   let data={
-    'path_project': "/Users/adrianabonilla/Documents/andes/pruebas/semana8/Android_worker"
+    'mutante':req.body.mutante,
+    'path_project': req.body.path_project
   }
   randomService.generatetest(data, function(apps){
       res.statusCode = 200;
@@ -18,9 +18,43 @@ router.post('/test', (req,res) => {
       res.send(err);
       
 })
+return res;
+});
 
+  
+router.post('/random', (req,res) => {
+  let data={
+    'mutante':req.body.mutante,
+    'path_project': req.body.path_project
+  }
+  randomService.generateRandom(data, function(apps){
+      res.statusCode = 200;
+      res.send({ status: "OK" });
+  },function(err){
+      res.statusCode = 404;
+      res.send(err);
+      
+})
 
 return res;
+});
+
+router.post('/vrt', (req,res) => {
+  let data={
+    'imagebefore': req.body.before,
+    'imageafter': req.body.after,
+    'path_project': req.body.path_project
+  }
+  randomService.generatevrt(data, function(apps){
+      res.statusCode = 200;
+      res.send({ status: "OK" });
+  },function(err){
+      res.statusCode = 404;
+      res.send(err);
+      
 })
+
+return res;
+});
 
 module.exports = router;
--- app/services/random.srv.js
+++ app/services/random.srv.js
@@ -4,19 +4,23 @@ const fs = require('fs');
 var resemblejsCompare = require('resemblejs').compare;
 
 module.exports.generatetest = function(req,success,error){
-    console.log(req.path_project)
-    shell.cd(`${req.path_project}/miliage`);
+    console.log(`${req.path_project}/miliage/com.evancharlton.mileage-mutant${req.mutante}`)
+    shell.cd(`${req.path_project}/miliage/com.evancharlton.mileage-mutant${req.mutante}`);
     shell.exec(`calabash-android gen`);
-    shell.exec(`calabash-android resign com.evancharlton.mileage.apk`)
-    shell.exec(`calabash-android run com.evancharlton.mileage.apk`, function(code, stdout, stderr) {
+    shell.exec(`calabash-android resign com.evancharlton.mileage_3110-aligned-debugSigned.apk`)
+     shell.exec(`calabash-android run com.evancharlton.mileage_3110-aligned-debugSigned.apk`, function(code, stdout, stderr) {
         console.log('Ok calabash');
-
-        var timestamp = (new Date()).getTime();
+      });
+ 
+    
+}
+module.exports.generatevrt = function(req,success,error){
+       var timestamp = (new Date()).getTime();
         var execution = {
             insertionDate: new Date(),
             timestamp: timestamp,
-            beforeImgUri: `${path_project}/original1.png`,
-            afterImgUri: `${path_project}/mutante1.png`
+            beforeImgUri: req.imagebefore,
+            afterImgUri: req.imageafter
         };
 
         const options = {
@@ -32,18 +36,24 @@ module.exports.generatetest = function(req,success,error){
                 else        {
                     console.log(data);
                 }                     
-                fs.writeFile(`${path_project}/comparation.png`, data.getBuffer(), (err) => {                
+                fs.writeFile(`${req.path_project}/comparation.png`, data.getBuffer(), (err) => {                
                     if (err) reject(err);
                     execution.comparation = data;
-                    execution.comparationImgUri = `${path_project}/comparation.png`;
+                    execution.comparationImgUri = `${req.path_project}/comparation.png`;
                     console.log('report '+JSON.stringify(execution))
                     success(execution);
                 });
             });
         
     });
+}
 
-      });
- 
-    
+module.exports.generateRandom = function(req,success,error){
+    console.log(`${req.path_project}/miliage/com.evancharlton.mileage-mutant${req.mutante}`)
+    shell.cd(`${req.path_project}/miliage/com.evancharlton.mileage-mutant${req.mutante}`);
+    shell.exec(`calabash-android gen`);
+    shell.exec(`calabash-android resign com.evancharlton.mileage_3110-aligned-debugSigned.apk`)
+    shell.exec(`adb unistall com.evancharlton.mileage_3110-aligned-debugSigned.apk`)
+    shell.exec(`adb install com.evancharlton.mileage_3110-aligned-debugSigned.apk`)
+    shell.exec(`adb shell monkey -p com.evancharlton.mileage -s 1576195569852 1000`)
 }
